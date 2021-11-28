91f3348e07759f0616dd9e76e63dc0192f1383b3
ah0y <nalfmalf@gmail.com>
2021-11-28 18:32:16 -0500

    *fixes issue where saved settings werent reflecting in slider properly *restricts css styling to just popup.html

--- popup.js
+++ popup.js
@@ -5,19 +5,19 @@ chrome.storage.sync.get({ green: 3, yellow: 1 }, function (result) {
   $("#green").text("green: " + green + "- 5");
   $("#yellow").text("yellow: " + yellow + "-" + green);
   $("#red").text("red: " + "0.5" + "-" + yellow);
-});
 
-$(".slider").slider({
-  min: 0.5,
-  max: 5,
-  range: true,
-  step: 0.1,
-  values: [1, 3],
-  slide: function (event, ui) {
-    $("#green").text("green: " + ui.values[1] + "- 5");
-    $("#yellow").text("yellow: " + ui.values[0] + "-" + ui.values[1]);
-    $("#red").text("red: " + "0.5" + "-" + ui.values[0]);
-    chrome.storage.sync.set({ yellow: ui.values[0] });
-    chrome.storage.sync.set({ green: ui.values[1] });
-  },
+  $(".slider").slider({
+    min: 0.5,
+    max: 5,
+    range: true,
+    step: 0.1,
+    values: [yellow, green],
+    slide: function (event, ui) {
+      $("#green").text("green: " + ui.values[1] + "- 5");
+      $("#yellow").text("yellow: " + ui.values[0] + "-" + ui.values[1]);
+      $("#red").text("red: " + "0.5" + "-" + ui.values[0]);
+      chrome.storage.sync.set({ yellow: ui.values[0] });
+      chrome.storage.sync.set({ green: ui.values[1] });
+    },
+  });
 });
